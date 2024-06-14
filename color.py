7613d5262858d60f6610fdac5e7de00a68c2a6ac
waqasbcs <waqasaliy2945@gmail.com>
2024-06-14 23:02:36 +0500

    color.py

---
+++ color.py
@@ -0,0 +1,14 @@
+from colorama import Fore
+
+print(f"{Fore.YELLOW}hello world")
+print(f"{Fore.RED}hello world")
+print(f"{Fore.BLUE}hello world")
+print(f"{Fore.CYAN}hello world")
+print(f"{Fore.GREEN}hello world")
+
+
+# 🟡 Yellow: hello world
+# 🔴 Red: hello world
+# 🔵 Blue: hello world
+# 🟦 Cyan: hello world
+# 🟢 Green: hello world
