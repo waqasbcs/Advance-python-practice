ea7dc19dc46bb760c50ba81de92ea333568b48ec
waqasbcs <waqasaliy2945@gmail.com>
2024-06-30 10:36:17 +0500

    Unpacking-Lists.py

---
+++ Unpacking-Lists.py
@@ -0,0 +1,6 @@
+numbers = [1, 2, 3, 4, 5]
+first, *middle, last = numbers
+
+print(f"First element: {first}")    # Output: First element: 1
+print(f"Middle elements: {middle}") # Output: Middle elements: [2, 3, 4]
+print(f"Last element: {last}")      # Output: Last element: 5
