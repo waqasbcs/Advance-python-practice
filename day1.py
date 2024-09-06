f21fedfbdf59f4a204e574232017990b2f06d077
waqasbcs <waqasaliy2945@gmail.com>
2024-09-06 12:55:50 +0500

    day1.py

---
+++ day1.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import numpy as np
+
+# Number of points
+num_points = 100
+
+# Generate random data
+x = np.random.rand(num_points) * 10
+y = np.random.rand(num_points) * 10
+colors = np.random.rand(num_points)
+
+# Create plot
+fig, ax = plt.subplots()
+scatter = ax.scatter(x, y, c=colors, s=50)
+
+# Update function for animation
+def animation_call(i):
+    x = np.random.rand(num_points) * 10
+    y = np.random.rand(num_points) * 10
+    scatter.set_offsets(np.c_[x, y])
+    return scatter,
+
+# Create animation
+anim = FuncAnimation(fig, animation_call, frames=200, interval=50)
+
+plt.show()
