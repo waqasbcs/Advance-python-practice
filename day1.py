b069a42c80d84e711654f89a702911927c0429cb
waqasbcs <waqasaliy2945@gmail.com>
2024-09-18 09:55:08 +0500

    day1.py

--- day1.py
+++ day1.py
@@ -30,3 +30,13 @@ plt.show()
 # books = Book.objects.select_related('author').all()
 # authors = Author.objects.prefetch_related('books').all()
 
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
+# from django.contrib.auth.models import User
+# from myapp.models import UserProfile
+
+# @receiver(post_save, sender=User)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         UserProfile.objects.create(user=instance)
+
